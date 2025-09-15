65c488ef4678a1f98d4d0bf96e3d93d67c4219f1
Tfactor2020@gmail.com <Tfactor08>
2025-09-15 17:06:35 +0300

    add db-specific annotations in Product model

--- src/main/java/com/torvaldsinc/linus-hostings/model/Product.java
+++ src/main/java/com/torvaldsinc/linus-hostings/model/Product.java
@@ -1,13 +1,18 @@
 package com.torvaldsinc.linushostings.model;
 
+import jakarta.persistence.*;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue
     private long id;
     private String name;
     private int cost;
