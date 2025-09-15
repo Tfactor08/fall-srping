3d2c14994b2ac9d92d7cc883464cb08f8c51c6ec
Tfactor2020@gmail.com <Tfactor08>
2025-09-15 17:04:05 +0300

    remove id field from request body

--- src/main/java/com/torvaldsinc/linus-hostings/api-tests/post-product.sh
+++ src/main/java/com/torvaldsinc/linus-hostings/api-tests/post-product.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -X POST http://localhost:8080/products -H "Content-Type: application/json" \
-    -d '{"id": 3, "name": "new-shit", "cost": 96}'
+    -d '{"name": "some-shit", "cost": 96}'
