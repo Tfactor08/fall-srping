ca0d422fbbbfc241a3a6a7c46cf798e1f0c4fec8
Tfactor2020@gmail.com <Tfactor08>
2025-09-16 22:02:40 +0300

    add Repository annotation

--- src/main/java/com/torvaldsinc/linus-hostings/repository/ProductRepository.java
+++ src/main/java/com/torvaldsinc/linus-hostings/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.torvaldsinc.linushostings.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.torvaldsinc.linushostings.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
