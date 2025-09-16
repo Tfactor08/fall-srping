94a8cf8736fc35fd12342256b7e3d2362d1d521c
Tfactor2020@gmail.com <Tfactor08>
2025-09-16 22:05:48 +0300

    add GET method for getting by id

--- src/main/java/com/torvaldsinc/linus-hostings/controller/ProductController.java
+++ src/main/java/com/torvaldsinc/linus-hostings/controller/ProductController.java
@@ -2,11 +2,10 @@ package com.torvaldsinc.linushostings.cotroller;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.torvaldsinc.linushostings.model.*;
 import com.torvaldsinc.linushostings.repository.ProductRepository;
@@ -14,17 +13,27 @@ import com.torvaldsinc.linushostings.repository.ProductRepository;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    @Autowired
-    private ProductRepository repo;
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @GetMapping
     public List<Product> getProducts() {
-        return repo.findAll();
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        return productRepository.findById(id)
+            .map(p -> ResponseEntity.ok(p))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProducts(@RequestBody Product product) {
-        Product savedProduct = repo.save(product);
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product savedProduct = productRepository.save(product);
         System.out.println("Product: " + savedProduct.getName());
         return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
     }
